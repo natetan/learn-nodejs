9ff23804ebe8f7379fe83569ade9924df9f59dcb
Yulong Tan <yulongtan67@gmail.com>
2016-12-20 14:56:46 -0800

    initial

---
+++ module1.js
@@ -0,0 +1,3 @@
+// Whatever gets exported from module2 is passed into the m2 variable
+var m2 = require('./module2');
+console.log(m2);
---
+++ module2.js
@@ -0,0 +1,4 @@
+var a = 1;
+
+module.exports.a = a;
+module.exports.b = 2;
