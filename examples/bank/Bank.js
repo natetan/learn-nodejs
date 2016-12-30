73169767dce565d55a87f6978fa111645c101086
Yulong Tan <yulongtan67@gmail.com>
2016-12-29 22:31:23 -0800

    fix bug in transfer

--- examples/bank/Bank.js
+++ examples/bank/Bank.js
@@ -29,7 +29,7 @@ function Bank(name, id, deposit) {
     }
   };
   this.transfer = function(bank, amount) {
-    if (this.balance >= 5 && amount < 0) {
+    if (this.balance >= 5 && amount > 0) {
       this.balance -= 5;
       if (amount > this.balance) {
         bank.balance += this.balance;
@@ -41,7 +41,7 @@ function Bank(name, id, deposit) {
       this.transactions++;
       bank.transactions++;
     }
-  }
+  };
 }
 
 function createBankAccount(name, id, deposit) {
