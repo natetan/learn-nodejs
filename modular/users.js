ca7aaf03e014c5f2e98db3140eaeda9c19603389
Yulong Tan <yulongtan67@gmail.com>
2016-12-29 17:57:40 -0800

    export person

---
+++ modular/users.js
@@ -0,0 +1,26 @@
+function printUsers() {
+  console.log('Here are some users');
+}
+
+function doNothingUseful() {
+  console.log('Does nothing useful');
+}
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.desc = function() {
+    if (this.age > 0) { 
+      console.log(this.name + ' is ' + this.age + ' years old');
+    } else {
+      console.log('You can\'t have a negative age, silly');
+    }
+  }
+}
+
+function createPerson(name, age) {
+  return new Person(name, age);
+}
+
+module.exports.print = printUsers;
+module.exports.createPerson = createPerson;
