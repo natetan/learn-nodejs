21d4bc4eeea301232e0deaa6b3c3568de6d98505
Yulong Tan <yulongtan67@gmail.com>
2016-12-30 17:00:45 -0800

    test default and non-default values for deposit

--- examples/bank/app.js
+++ examples/bank/app.js
@@ -1,6 +1,6 @@
 var Bank = require('./Bank');
 let per = Bank.createBankAccount('Percy', 0, 500);
-let ann = Bank.createBankAccount('Annabeth', 1, 1000);
+let ann = Bank.createBankAccount('Annabeth', 1);
 console.log(per.getBalance());
 console.log('Percy balance: ' + per.balance);
 console.log('Annabeth balance: ' + ann.balance);
